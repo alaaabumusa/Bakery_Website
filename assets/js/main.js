889dce4736b7cd68cfbd03200912952d9e606295
Alaa Abu Mussa <166786217+alaaabumusa@users.noreply.github.com>
2024-11-14 22:03:12 +0200

    Upload updated assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,28 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
 const countDown = () => {
-    const countDownDate = new Date("2025-2-5 23:59:59").getTime();
-    const now = new Date().getTime();
-    const distance = countDownDate - now;
-  
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    document.querySelector(".days").textContent = days;
-    document.querySelector(".hours").textContent = hours;
-    document.querySelector(".minutes").textContent = minutes;
-    document.querySelector(".seconds").textContent = seconds;
-  };
-  
-  setInterval(() => {
-    countDown();
-  }, 1000);
+  const countDownDate = new Date("2025-2-5 23:59:59").getTime();
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").textContent = days;
+  document.querySelector(".hours").textContent = hours;
+  document.querySelector(".minutes").textContent = minutes;
+  document.querySelector(".seconds").textContent = seconds;
+};
+
+setInterval(() => {
+  countDown();
+}, 1000);
